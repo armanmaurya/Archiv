9ea983a7244fc6df89aa3a69ff9e5b78e1fb6a78
Arman Maurya <armanmarya6@gmail.com>
2026-04-20 20:07:12 +0530

    backup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation("org.opencv:opencv:4.10.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
