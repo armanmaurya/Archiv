e43850adb9224676b36241980c71f10c1a115b17
Arman Maurya <armanmarya6@gmail.com>
2026-04-23 22:28:55 +0530

    add: multiple language feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation("org.opencv:opencv:4.10.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
