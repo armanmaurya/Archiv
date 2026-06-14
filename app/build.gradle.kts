b88680a09c85c1220b39719c82af1b488a9aaabd
Arman Maurya <mauryaarman5@gmail.com>
2026-06-14 18:38:40 +0530

    chore: bump to version 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.armanmaurya.archiv"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.4"
+        versionCode = 7
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
